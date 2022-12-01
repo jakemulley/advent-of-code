10fe52f3e37b059d724f64157a997a24de33e1c8
jakemulley <jakemulley@users.noreply.github.com>
2022-12-01 13:17:17 +0000

    Day 1 - Part 2

--- 2022/12/01/answer.js
+++ 2022/12/01/answer.js
@@ -13,6 +13,18 @@ const totalCaloriesPerElf = elves.map(calories => {
   }, 0)
 })
 
+// Part One
 const highest = Math.max(...totalCaloriesPerElf)
 
-console.log(highest)
+console.log(`Part 1 => ${highest}`)
+
+// Part Two
+const topThree = totalCaloriesPerElf.sort((a, b) => {
+  return b - a;
+}).slice(0, 3)
+
+const topThreeSum = topThree.reduce((accumulator, integer) => {
+  return Number.parseInt(accumulator + integer)
+}, 0)
+
+console.log(`Part 2 => ${topThreeSum}`)
