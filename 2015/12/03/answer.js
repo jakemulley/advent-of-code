7109d227449ecd9d752dee4a5bd80c1cec87b03f
jakemulley <jakemulley@users.noreply.github.com>
2023-12-05 17:18:55 +0000

    2015-12-{02,03,06}: refactor answers

--- 2015/12/03/answer.js
+++ 2015/12/03/answer.js
@@ -1,82 +1,67 @@
 import { getInput } from "../../../utils/helpers.js";
 const input = (await getInput(import.meta.url)).join().split("");
 
-// Part 1
-const part1HousesVisited = {};
-const part1Prepare = input.reduce(
-  (accumulator, currentValue) => {
-    if (currentValue === "^") {
-      accumulator.y += 1;
-    }
-    if (currentValue === "v") {
-      accumulator.y -= 1;
-    }
-    if (currentValue === "<") {
-      accumulator.x += 1;
-    }
-    if (currentValue === ">") {
-      accumulator.x -= 1;
-    }
-    if (!part1HousesVisited[`${accumulator.x},${accumulator.y}`]) {
-      part1HousesVisited[`${accumulator.x},${accumulator.y}`] = 1;
-    }
-    part1HousesVisited[`${accumulator.x},${accumulator.y}`] += 1;
-    return accumulator;
-  },
-  {
-    x: 0,
-    y: 0,
-  }
-);
+function processDirections(input, hasRobotSanta) {
+  const housesVisited = {};
 
-const part1 = Object.keys(part1HousesVisited).filter(
-  (house) => part1HousesVisited[house] >= 1
+  input.reduce(
+    (accumulator, currentValue, index) => {
+      let who = "santa";
+
+      if (hasRobotSanta && index % 2 === 0) {
+        who = "robosanta";
+      }
+
+      if (currentValue === "^") {
+        accumulator[who].y += 1;
+      }
+      if (currentValue === "v") {
+        accumulator[who].y -= 1;
+      }
+      if (currentValue === "<") {
+        accumulator[who].x += 1;
+      }
+      if (currentValue === ">") {
+        accumulator[who].x -= 1;
+      }
+
+      if (!housesVisited[`${accumulator[who].x},${accumulator[who].y}`]) {
+        housesVisited[`${accumulator[who].x},${accumulator[who].y}`] = 1;
+      }
+
+      housesVisited[`${accumulator[who].x},${accumulator[who].y}`] += 1;
+
+      return accumulator;
+    },
+    {
+      santa: {
+        x: 0,
+        y: 0,
+      },
+      robosanta: {
+        x: 0,
+        y: 0,
+      },
+    }
+  );
+
+  return housesVisited;
+}
+
+// Part 1
+const part1Visited = processDirections(input, false);
+
+const part1 = Object.keys(part1Visited).filter(
+  (house) => part1Visited[house] >= 1
 ).length;
 
 console.log("Answer one:", part1);
 
 // Part 2
-const part2HousesVisited = {};
-const part2Prepare = input.reduce(
-  (accumulator, currentValue, index) => {
-    let who = "santa";
+const part2Visited = processDirections(input, true);
 
-    if (index % 2 === 0) {
-      who = "robosanta";
-    }
-
-    if (currentValue === "^") {
-      accumulator[who].y += 1;
-    }
-    if (currentValue === "v") {
-      accumulator[who].y -= 1;
-    }
-    if (currentValue === "<") {
-      accumulator[who].x += 1;
-    }
-    if (currentValue === ">") {
-      accumulator[who].x -= 1;
-    }
-    if (!part2HousesVisited[`${accumulator[who].x},${accumulator[who].y}`]) {
-      part2HousesVisited[`${accumulator[who].x},${accumulator[who].y}`] = 1;
-    }
-    part2HousesVisited[`${accumulator[who].x},${accumulator[who].y}`] += 1;
-    return accumulator;
-  },
-  {
-    santa: {
-      x: 0,
-      y: 0,
-    },
-    robosanta: {
-      x: 0,
-      y: 0,
-    },
-  }
-);
-
-const part2 = Object.keys(part2HousesVisited).filter(
-  (house) => part2HousesVisited[house] >= 1
+const part2 = Object.keys(part2Visited).filter(
+  (house) => part2Visited[house] >= 1
 ).length;
 
 console.log("Answer two:", part2);
